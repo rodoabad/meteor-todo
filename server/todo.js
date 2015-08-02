db52282f21d463f8c2b5824c3873e5fe589f4f22
rodoabad <rodoabad@gmail.com>
2015-08-02 13:50:40 -0500

    Moved files into their own folders.

---
+++ server/todo.js
@@ -0,0 +1,12 @@
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish('tasks', function () {
+    return Tasks.find({
+      $or: [
+        {private: {$ne: true}},
+        {owner: this.userId}
+      ]
+    });
+  });
+}
